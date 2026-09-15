bd22ad64504cc51da7825f6cd3a0b8e8947baf37
Ankit Tharol <tharolankit19@gmail.com>
2026-09-15 12:48:30 +0530

    Add Kryx launch film scaffold: launch-film/src/Root.tsx

---
+++ launch-film/src/Root.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Composition} from "remotion";
+import {KryxLaunch} from "./KryxLaunch";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="KryxLaunch-16x9"
+        component={KryxLaunch}
+        durationInFrames={1620}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{vertical:false}}
+      />
+      <Composition
+        id="KryxLaunch-9x16"
+        component={KryxLaunch}
+        durationInFrames={1620}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{vertical:true}}
+      />
+    </>
+  );
+};
