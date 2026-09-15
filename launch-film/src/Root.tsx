8e6b1d7ccb9844137a5e423c710b22ada8afada6
Ankit Tharol <tharolankit19@gmail.com>
2026-09-15 19:45:34 +0530

    Extend Kryx launch film for complete product walkthrough

--- launch-film/src/Root.tsx
+++ launch-film/src/Root.tsx
@@ -8,7 +8,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="KryxLaunch-16x9"
         component={KryxLaunch}
-        durationInFrames={1620}
+        durationInFrames={2010}
         fps={30}
         width={1920}
         height={1080}
@@ -17,7 +17,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="KryxLaunch-9x16"
         component={KryxLaunch}
-        durationInFrames={1620}
+        durationInFrames={2010}
         fps={30}
         width={1080}
         height={1920}
